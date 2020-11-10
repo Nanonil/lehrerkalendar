eba2d1fb05697857abf3556f43040b92506ce994
OliW3r <xxoliwerxx@gmail.com>
2020-11-10 14:57:54 +0100

    Update models.py
    add unique constraints and comments to model

--- backend/src/kalender/models.py
+++ backend/src/kalender/models.py
@@ -6,24 +6,24 @@ from django.db import models
   #<coloumn_name> = models.<field_type>(<restrictions>)
 
 class Teacher(models.Model):
-    TeacherName = models.CharField(max_length=80)
+    TeacherName = models.CharField(max_length=80) # Names will be limited to 80 characters
 
 class Day(models.Model):
 	DateOfDay = models.DateField()
-	Weekday = models.IntegerField()
+	Weekday = models.IntegerField() # int from 1 to 5; 1 for Monday, 2 Tuesday, ..., 5 Friday
 
 class Schoolclass(models.Model):
-	ClassName = models.CharField()
+	ClassName = models.CharField(max=5) # Class Names are always 5 characters long (eg "FIA85")
 
 class Students(models.Model):
-	StudentName = models.CharField(max_length=80)
+	StudentName = models.CharField(max_length=80) # Names will be limited to 80 characters
 	ClassID = models.ForeignKey(Schoolclass, on_delete=models.CASCADE)
 
 class Lesson(models.Model):
 	DayID = models.ForeignKey(Day, on_delete=models.CASCADE)
 	ClassID = models.ForeignKey(Schoolclass, on_delete=models.CASCADE)
 	Subject = models.CharField(max_length=50)
-	Period = models.IntegerField()
+	Period = models.IntegerField() # int from 1 to 15; 1 '07:45-8:30'
 	Content = models.TextField()
 	Note = models.TextField()
 
@@ -31,7 +31,11 @@ class StudentGrading(models.Model):
 	StudentID = models.ForeignKey(Students, on_delete=models.CASCADE)
 	TeacherID = models.ForeignKey(Teacher, on_delete=models.CASCADE)
 	LessonID = models.ForeignKey(Lesson, on_delete=models.CASCADE)
-	Grading = models.IntegerField()
+	Grading = models.IntegerField() # int from 1 to 5
+	class Meta:
+		constraints = [
+			models.UniqueConstraint(fields=['StudentID', 'TeacherID', 'LessonID'], name='StudentGradingUnique')
+		]
 
 class Schedule(models.Model):
 	TeacherID = models.ForeignKey(Teacher, on_delete=models.CASCADE)
@@ -42,3 +46,7 @@ class Schedule(models.Model):
 	Wednesday = models.ForeignKey(Day, on_delete=models.CASCADE)
 	Thursday = models.ForeignKey(Day, on_delete=models.CASCADE)
 	Friday = models.ForeignKey(Day, on_delete=models.CASCADE)
+	class Meta:
+		constraints = [
+			models.UniqueConstraint(fields=['TeacherID', 'CalendarWeek', 'Year'], name='UniqueSchedule')
+		]
